38025e5847edf1424cb3be828e675ac14ede1569
ayakuma309 <ayako.30901@gmail.com>
2023-05-16 11:23:22 +0900

    add: firebase設定

---
+++ lib/firebase.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import { getFirestore } from 'firebase/firestore'
+import {getAuth, GoogleAuthProvider} from "firebase/auth";
+
+import 'firebase/analytics'
+import 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: "self-introduction-9b180",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app)
+export { auth, provider, db};
