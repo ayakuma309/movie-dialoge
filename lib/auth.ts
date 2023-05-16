1d7e064713dcb5febdf6d9615ef1a3f05905268d
ayakuma309 <ayako.30901@gmail.com>
2023-05-16 12:28:39 +0900

    ✨ add: ログイン, ログアウト, ユーザ認証の管理を実装する

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,55 @@
-import { getAuth, signInWithRedirect, signOut, GoogleAuthProvider } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { atom, useRecoilValue, useSetRecoilState } from "recoil";
+import {
+  User,
+  getAuth,
+  signInWithRedirect,
+  signOut,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
+// null の場合はログインしていない状態
+type UserState = User | null;
+
+const userState = atom<UserState>({
+  key: "userState",
+  default: null,
+  dangerouslyAllowMutability: true,
+});
+
+//login
 export const login = (): Promise<void> => {
   const provider = new GoogleAuthProvider();
   const auth = getAuth();
   return signInWithRedirect(auth, provider);
 };
 
+//logout
 export const logout = (): Promise<void> => {
   const auth = getAuth();
   return signOut(auth);
+};
+
+// ユーザー情報取得
+export const useAuth = (): boolean => {
+	//isLoading は onAuthStateChanged() を実行中か確認するための状態
+  const [isLoading, setIsLoading] = useState(true);
+  const setUser = useSetRecoilState(userState);
+
+  useEffect(() => {
+    const auth = getAuth();
+		//ユーザ認証を監視し,変更があったときに引数のコールバック関数を実行
+    return onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setIsLoading(false);
+    });
+  }, [setUser]);
+
+  return isLoading;
+};
+
+//useUser() はその UserState を他のコンポーネントで呼び出すための関数
+export const useUser = (): UserState => {
+  return useRecoilValue(userState);
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import '../lib/firebase'
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+import { RecoilRoot } from "recoil";
+import { useAuth } from "../lib/auth";
+
+type Props = {
+  children: JSX.Element;
+};
+
+//useAuth() は <RecoilRoot> の子孫コンポーネントでしか使用できないので
+//<Auth> コンポーネントを作成し, その中で使用しています
+const Auth = ({ children }: Props): JSX.Element => {
+  const isLoading = useAuth();
+
+  return isLoading ? <p>Loading...</p> : children;
+};
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <Auth>
+        <Component {...pageProps} />
+      </Auth>
+    </RecoilRoot>
+  );
 }
+
+export default App;
