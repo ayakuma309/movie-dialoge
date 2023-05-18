49ac74cecb8a8bfbfc0867f1037be9681063f974
ayakuma309 <ayako.30901@gmail.com>
2023-05-18 10:50:01 +0900

    ✨ add: Headerの追加

--- pages/users/[id]/index.tsx
+++ pages/users/[id]/index.tsx
@@ -1,18 +1,23 @@
+import Layout from "@/pages/components/common/Layout";
 import {  useUser } from "../../../lib/auth";
 
 export default function UserPage() {
   const user = useUser();
   return (
-    <div>
-      {user ? (
-        <div>
-          <p>{user.uid}</p>
-          <p>{user.displayName}</p>
+    <Layout title="マイページ">
+      <div className="container mx-auto flex flex-col items-center sm:max-w-7xl">
+        <div className="mt-24">
+          {user ? (
+            <div>
+              <p>{user.uid}</p>
+              <p>{user.displayName}</p>
+            </div>
+          ) : (
+            <p>ロード中…</p>
+          )}
         </div>
-      ) : (
-        <p>ロード中…</p>
-      )}
-    </div>
+      </div>
+    </Layout>
   )
 }
 
