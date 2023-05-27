17462f0abcb9c476a06aa663786921a6a6db0084
ayakuma309 <ayako.30901@gmail.com>
2023-05-27 18:57:26 +0900

    add: カードの中のセリフをランダムに表示

--- components/dialogue/AllDialogue.tsx
+++ components/dialogue/AllDialogue.tsx
@@ -5,7 +5,8 @@ import DialogueModal from './DialogueModal';
 
 
 const AllDialogue = () => {
-  const [movies, setMovies] = useState<AllDialogueProps[]>([]); // Movie型を使用する
+  const [movies, setMovies] = useState<AllDialogueProps[]>([]);
+  const [randomMovie, setRandomMovie] = useState<AllDialogueProps>();
   const db = getFirestore();
 
   useEffect(() => {
@@ -19,22 +20,45 @@ const AllDialogue = () => {
         console.error('Error fetching movies: ', error);
       }
     };
-
     fetchMovies();
   }, []);
 
+  useEffect(() => {
+    if (movies.length > 0) {
+      const shuffledmovies = [...movies];
+      for (let i = shuffledmovies.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledmovies[i], shuffledmovies[j]] = [shuffledmovies[j], shuffledmovies[i]];
+      }
+      setRandomMovie(shuffledmovies[0]);
+    }
+  }, [movies]);
 
   return (
-    <div className='mt-5'>
-      {movies.map((movie) => (
-        <div key={movie.id}>
-          <DialogueModal
-            title={movie.title}
-            dialogue={movie.dialogue}
-            poster_path={movie.poster_path}
-          />
+    <div className='dialogue'>
+      <div className="container mx-auto">
+        <div className="card">
+          <div className="card__content">
+            <p className="description">
+              {randomMovie?.dialogue}
+            </p>
+            <p className="title text-gray-600 mt-3">
+              <small>{randomMovie?.title}より</small>
+            </p>
+          </div>
         </div>
-      ))}
+      </div>
+      <div className='mt-24'>
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <DialogueModal
+              title={movie.title}
+              dialogue={movie.dialogue}
+              poster_path={movie.poster_path}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
