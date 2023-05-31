cb93389bdd7775808a68b89ad43b1fde8be18ec7
ayakuma309 <ayako.30901@gmail.com>
2023-05-31 14:40:08 +0900

    add: 修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
             </>
           ) : (
             <>
-              <Auth />
+              <p>ログインしてない</p>
             </>
           )}
         </div>
