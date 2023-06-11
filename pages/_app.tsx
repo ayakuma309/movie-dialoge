21ace19abd90f2b09f01c2ecaeb81166c0da3a97
ayakuma309 <ayako.30901@gmail.com>
2023-06-11 22:46:18 +0900

    fix: 修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
+
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import '../lib/firebase'
 
 import { RecoilRoot } from "recoil";
 import { useAuth } from "../lib/auth";
-
-import Script from "next/script";
-import * as gtag from "../lib/gtag";
-
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 const router = useRouter();
 
