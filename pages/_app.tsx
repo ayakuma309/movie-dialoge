535e05d313681b3ace125a95685e4ccd65e44fc5
ayakuma309 <ayako.30901@gmail.com>
2023-06-11 22:33:03 +0900

    add: GA導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,24 @@ import '../lib/firebase'
 import { RecoilRoot } from "recoil";
 import { useAuth } from "../lib/auth";
 
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
+
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+
+
 type Props = {
   children: JSX.Element;
 };
@@ -20,6 +38,22 @@ const Auth = ({ children }: Props): JSX.Element => {
 function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
       <Auth>
         <Component {...pageProps} />
       </Auth>
