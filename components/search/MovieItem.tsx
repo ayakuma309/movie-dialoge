b2000c31bd9731f837afa08db163a3f2762d841d
ayakuma309 <ayako.30901@gmail.com>
2023-06-11 20:04:53 +0900

    fix: 使ってないコードを削除

--- components/search/MovieItem.tsx
+++ components/search/MovieItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { MovieItemProps } from '../../types/MovieTypes';
 import MovieModal from './MovieModal';
 import { NextPage } from 'next';
