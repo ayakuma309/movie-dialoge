b716478496bda7f8294cc1c9d83de6891e9161d9
ayakuma309 <ayako.30901@gmail.com>
2023-06-19 15:01:11 +0900

    fix: プライバシポリシーの追加

---
+++ components/common/Footer.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+
+const Footer: React.FC =() => {
+  return (
+    <footer className="text-white body-font">
+      <div className="container mx-auto flex flex-col">
+        <div className="flex flex-col text-center">
+          <nav className="list-none mb-1">
+            <li>
+              <Link
+                href="/terms"
+              >
+                利用規約
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/privacyPolicy"
+              >
+                プライバシーポリシー
+              </Link>
+            </li>
+          </nav>
+          <p className="text-sm text-center mt-2">
+            © 2023 ScriptSelectMovies
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from './Header'
+import Footer  from './Footer';
 
 type MyComponentProps = {
   children: React.ReactNode
@@ -11,23 +12,26 @@ const Layout: React.FC<MyComponentProps> = ({
   title = 'ScriptSelectMovies',
 }) => {
   return (
-    <div className='mx-auto flex min-h-screen flex-col items-center justify-center'>
-      <Head>
-        <title>{title}</title>
-      </Head>
-      <header>
-        <section id='header'>
-          <Header />
-        </section>
-      </header>
-      <main className='flex max-w-screen-sm flex-1 sm:w-screen sm:max-w-screen-xl'>
-        <div className="container mx-auto flex flex-col items-center sm:max-w-7xl">
-          <div className="mt-24">
-            {children}
+    <>
+      <div className='mx-auto flex min-h-screen flex-col items-center justify-center'>
+        <Head>
+          <title>{title}</title>
+        </Head>
+        <header>
+          <section id='header'>
+            <Header />
+          </section>
+        </header>
+        <main className='flex max-w-screen-sm flex-1 sm:w-screen sm:max-w-screen-xl'>
+          <div className="container mx-auto flex flex-col items-center sm:max-w-7xl">
+            <div className="mt-24">
+              {children}
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+      <Footer/>
+    </>
   )
 }
 export default Layout
