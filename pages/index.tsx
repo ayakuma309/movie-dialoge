1da9d6d47fee3f6f1f2402d31d59f2fdc8b27252
ayakuma309 <ayako.30901@gmail.com>
2023-06-21 09:25:50 +0900

    style: 追加ボタンのレイアウトを修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { useUser } from "../lib/auth";
 import Layout from "../components/common/Layout";
 import AllDialogue from "@/components/dialogue/AllDialogue";
 import Link from "next/link";
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+import AddIcon from '@mui/icons-material/Add';
 const Home: NextPage = () => {
   const user = useUser();
   return (
@@ -13,11 +13,11 @@ const Home: NextPage = () => {
           <>
             <div className="flex items-center justify-center">
               {user !== null && (
-                <Link href="/search">
-                  <button className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2">
-                    <AddCircleIcon />
-                  </button>
-                </Link>
+                <div className="float_btn">
+                  <Link href="/search">
+                    <AddIcon className="fa-plus"/>
+                  </Link>
+                </div>
               )}
             </div>
             <AllDialogue />
