f7f5b13d8e6736e9f42fa64e74ae058ce273a876
ayakuma309 <ayako.30901@gmail.com>
2023-06-22 21:24:19 +0900

    add: OGP画像,favicon画像 #30

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -14,9 +14,35 @@ const Layout: React.FC<MyComponentProps> = ({
   return (
     <>
       <div className='mx-auto flex min-h-screen flex-col items-center justify-center'>
-        <Head>
-          <title>{title}</title>
-        </Head>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content="映画の宝さがし" />
+        <meta
+          name="og:title"
+          property="og:title"
+          content={title}
+        />
+        <meta
+          name="description"
+          content="映画の宝さがし"
+        />
+        <meta
+          property="og:image"
+          key="ogImage"
+          content={`https://movie-dialoge.vercel.app/ogp.png`}
+        />
+        <meta
+          name="twitter:card"
+          key="twitterCard"
+          content="summary_large_image"
+        />
+        <meta
+          name="twitter:image"
+          key="twitterImage"
+          content={`https://movie-dialoge.vercel.app/ogp.png`}
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
         <header>
           <section id='header'>
             <Header />
