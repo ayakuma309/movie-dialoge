1dca49ce17fa246e92f8a9fd7a593242bb5a1a2f
ayakuma309 <ayako.30901@gmail.com>
2023-06-26 11:37:58 +0900

    fix: バグの修正

--- components/common/PageHead.tsx
+++ components/common/PageHead.tsx
@@ -7,13 +7,13 @@ type PageHeadProps = {
 const PageHead: React.FC<PageHeadProps> = ({ title }) => (
   <Head>
     <title>{title}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="映画の宝さがし" />
-    <meta name="og:title" property="og:title" content={title} />
+    <meta property="og:title" content={title} />
     <meta property="og:description" content="映画の宝さがし" />
-    <meta property="og:image" key="ogImage" content={`https://movie-dialoge.vercel.app/ogp.png`}/>
-    <meta name="twitter:card" key="twitterCard" content="summary_large_image"/>
-    <meta name="twitter:image" key="twitterImage" content={`https://movie-dialoge.vercel.app/ogp.png`}/>
+    <meta property="og:image" key="ogImage" content="https://movie-dialoge.vercel.app/ogp.png" />
+    <meta name="twitter:card" key="twitterCard" content="summary_large_image" />
+    <meta name="twitter:image" key="twitterImage" content="https://movie-dialoge.vercel.app/ogp.png" />
     <link rel="icon" href="/favicon.ico" />
   </Head>
 );
