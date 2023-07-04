045969f298c09b7e857eea899a0121e8a9bc1644
ayakuma309 <ayako.30901@gmail.com>
2023-07-04 14:13:58 +0900

    fix: firebaseエラーが出るため修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,24 @@ import { useRouter } from "next/router";
 
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
+import { useAuth } from "@/lib/auth";
 
+
+type Props = {
+  children: JSX.Element;
+};
+
+//useAuth() は <RecoilRoot> の子孫コンポーネントでしか使用できないので
+//<Auth> コンポーネントを作成し, その中で使用しています
+const Auth = ({ children }: Props): JSX.Element => {
+  const isLoading = useAuth();
+
+  return isLoading ? (
+    <div className="my-24 mx-auto text-center" style={{  height: '500px' }}>
+      loading...
+    </div>
+    ) : children;
+};
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -30,37 +47,39 @@ function App({ Component, pageProps }: AppProps) {
 
   return (
     <RecoilRoot>
-      <>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      <Auth>
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+              `,
+            }}
+          />
+          <Component {...pageProps} />
+          <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable={false}
+          pauseOnHover={false}
+          theme="light"
         />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_MEASUREMENT_ID}');
-            `,
-          }}
-        />
-        <Component {...pageProps} />
-        <ToastContainer
-        position="top-center"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable={false}
-        pauseOnHover={false}
-        theme="light"
-      />
-      </>
+        </>
+      </Auth>
     </RecoilRoot>
   );
 }
--- pages/dialogueIndex.tsx
+++ pages/dialogueIndex.tsx
@@ -1,50 +1,33 @@
 import { NextPage } from "next";
-import { useAuth, useUser } from "../lib/auth";
+import { useUser } from "../lib/auth";
 
 import Layout from "../components/common/Layout";
 import AllDialogue from "@/components/dialogue/AllDialogue";
 import Link from "next/link";
 import AddIcon from '@mui/icons-material/Add';
 
-type Props = {
-  children: JSX.Element;
-};
-
-//useAuth() は <RecoilRoot> の子孫コンポーネントでしか使用できないので
-//<Auth> コンポーネントを作成し, その中で使用しています
-const Auth = ({ children }: Props): JSX.Element => {
-  const isLoading = useAuth();
-
-  return isLoading ? (
-    <div className="my-24 mx-auto text-center" style={{  height: '500px' }}>
-      loading...
-    </div>
-    ) : children;
-};
 
 const dialogueIndex: NextPage = () => {
   const user = useUser();
   return (
-    <Auth>
-      <Layout title="セリフの映画館">
-        <div className="movie-dialogue">
-          <div>
-            <>
-              <div className="flex items-center justify-center">
-                {user !== null && (
-                  <div className="float_btn">
-                    <Link href="/search">
-                      <AddIcon className="fa-plus"/>
-                    </Link>
-                  </div>
-                )}
-              </div>
-              <AllDialogue />
-            </>
-          </div>
+    <Layout title="セリフの映画館">
+      <div className="movie-dialogue">
+        <div>
+          <>
+            <div className="flex items-center justify-center">
+              {user !== null && (
+                <div className="float_btn">
+                  <Link href="/search">
+                    <AddIcon className="fa-plus"/>
+                  </Link>
+                </div>
+              )}
+            </div>
+            <AllDialogue />
+          </>
         </div>
-      </Layout>
-    </Auth>
+      </div>
+    </Layout>
   );
 };
 export default dialogueIndex;
